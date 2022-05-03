cae7982554573b0c5e09bd8d39ef7349e6b032bd
Shivang Garg <shivang10garg@gmail.com>
2022-05-03 22:00:19 +0530

    ✨ allows cell to be marked as maybe_mine, adds header and improves functionality

--- cell.py
+++ cell.py
@@ -2,6 +2,7 @@ from tkinter import Button, Label
 import random
 
 import settings
+import sys
 
 
 class Cell:
@@ -15,6 +16,7 @@ class Cell:
         self.x = x
         self.y = y
         self.is_opened = False
+        self.is_mine_candidate = False
 
         # append the object to the Cell.all list
         Cell.all.append(self)
@@ -50,10 +52,17 @@ class Cell:
                 for cell_obj in self.surrounded_cells:
                     cell_obj.show_cell()
             self.show_cell()
+            # If mines count is equal to the cells left count, player won
+            if Cell.cell_count == settings.MINES_COUNT:
+                print("you won")
+        # Cancel left and right click events if cell is already opened:
+        self.cell_btn_object.unbind("<Button-1>")
+        self.cell_btn_object.unbind("<Button-3>")
 
     def show_mine(self):
-        # a logic to interrupt a game and display a message that player lost
         self.cell_btn_object.configure(bg="red")
+        # ctypes.windll.user32.MessageBoxW(0, "You clicked on a mine", "Game Over")
+        sys.exit()
 
     def get_cell_by_axis(self, x, y):
         # return a cell object based on the value of x y
@@ -93,12 +102,25 @@ class Cell:
                 Cell.cell_count_label_object.configure(
                     text=f"Cells Left:{Cell.cell_count}"
                 )
+            # If this was a mine candidate, then for safety, we should
+            # configure the background color to SystemButtonFace
+            self.cell_btn_object.configure(
+                bg="gray85"
+            )
         # Mark the cell as opened (Use is as the last line of this method)
         self.is_opened = True
 
     def right_click_actions(self, event):
-        print(event)
-        print("I am right clicked!")
+        if not self.is_mine_candidate:
+            self.cell_btn_object.configure(
+                bg="orange"
+            )
+            self.is_mine_candidate = True
+        else:
+            self.cell_btn_object.configure(
+                bg="gray85"
+            )
+            self.is_mine_candidate = False
 
     @staticmethod
     def randomize_mines():
